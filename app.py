e1ce51bd665f52f65e051f4a1c6fc41aeeaa49f4
Anthony Ikeda <anthony.ikeda@gmail.com>
2022-12-31 05:12:41 +0000

    Testing out some features of Flask and Python

--- app.py
+++ app.py
@@ -1,7 +1,23 @@
-from flask import Flask, render_template
+""" Entry point to application """
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return render_template("index.html", title="Hello")
+    """ Index page """
+    return render_template("index.html", title="First Python Application", name="Person A")
+
+@app.route("/input")
+def info_collect():
+    """ Collect some basic data """
+    return render_template("input.html", message="Please enter your details")
+
+@app.route("/record", methods=['POST'])
+def info_submission():
+    """ Read the submitted data """
+    firstname = request.form["firstname"]
+    lastname = request.form["lastname"]
+    age = request.form["age"]
+    print(f'User is {firstname}, {lastname} and their age is {age}')
+    return render_template("input.html", message="Done")
